e811c88b5966fa8008ae1fedf15dad55a46d3f26
adelgado <adelgado0723@gmail.com>
2024-09-29 02:48:46 -0400

    cleaning up generated command code

--- src/commands/tcghelper/setanalysis.ts
+++ src/commands/tcghelper/setanalysis.ts
@@ -4,18 +4,18 @@ import { analyzeCurrentPricesBySets } from '../../pricing'
 
 export default class TcghelperSetanalysis extends Command {
   static override args = {
-    file: Args.string({description: 'file to read'}),
+    /* file: Args.string({description: 'file to read'}), */
   }
 
-  static override description = 'describe the command here'
+  static override description = 'gets total inventory value by set ordered from highest to lowest value'
 
   static override examples = ['<%= config.bin %> <%= command.id %>']
 
   static override flags = {
     // flag with no value (-f, --force)
-    force: Flags.boolean({char: 'f'}),
+    /* force: Flags.boolean({char: 'f'}), */
     // flag with a value (-n, --name=VALUE)
-    name: Flags.string({char: 'n', description: 'name to print'}),
+    /* name: Flags.string({char: 'n', description: 'name to print'}), */
   }
 
   public async run(): Promise<void> {
@@ -23,9 +23,9 @@ export default class TcghelperSetanalysis extends Command {
 
     const name = flags.name ?? 'world'
     this.log(`hello ${name} from /root/dev/tcghelper/src/commands/tcghelper/setanalysis.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
-    }
+    /* if (args.file && flags.force) { */
+    /*   this.log(`you input --force and --file: ${args.file}`) */
+    /* } */
 
     await analyzeCurrentPricesBySets();
 
--- src/commands/tcghelper/updateprices.ts
+++ src/commands/tcghelper/updateprices.ts
@@ -4,18 +4,18 @@ import {updatePrices} from '../../pricing'
 
 export default class TcghelperUpdatePrices extends Command {
   static override args = {
-    file: Args.string({description: 'file to read'}),
+    /* file: Args.string({description: 'file to read'}), */
   }
 
-  static override description = 'describe the command here'
+  static override description = 'reads prices and creates and updates them to the lowest in the market'
 
   static override examples = ['<%= config.bin %> <%= command.id %>']
 
   static override flags = {
     // flag with no value (-f, --force)
-    force: Flags.boolean({char: 'f'}),
+    /* force: Flags.boolean({char: 'f'}), */
     // flag with a value (-n, --name=VALUE)
-    name: Flags.string({char: 'n', description: 'name to print'}),
+    /* name: Flags.string({char: 'n', description: 'name to print'}), */
   }
 
   public async run(): Promise<void> {
@@ -23,9 +23,9 @@ export default class TcghelperUpdatePrices extends Command {
 
     const name = flags.name ?? 'world'
     this.log(`hello ${name} from /root/dev/tcghelper/src/commands/tcghelper/updatePrices.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
-    }
+    /* if (args.file && flags.force) { */
+    /*   this.log(`you input --force and --file: ${args.file}`) */
+    /* } */
 
     await updatePrices()
   }
